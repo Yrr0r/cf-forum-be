4081ad1f4f6983c628c14906ebf46d60ff1d8947
Steve <yrr0r.g@gmail.com>
2021-07-27 13:56:13 +0800

    discussion thread almost done

--- src/discuss.mjs
+++ src/discuss.mjs
@@ -13,7 +13,9 @@ export async function handleDiscuss(request, env){
 		return readThread(task, env);
 	} else if(task.method == 'create'){
 		return createThread(task, env);
-	} 
+	} else if(task.method == 'reply'){
+		return replyThread(task, env);
+	}
 	// todo: handle other exception accesses
 }
 
@@ -26,6 +28,8 @@ async function readThread(task, env){
 	}
 	//check if range is specified.
 	if(task['range'] == undefined){
+		// todo: be able to response a specified range.
+		// current: replying last 100 messages.
 		//return new Response("Error: Must specify a range.", {status:404});
 	}
 	//todo: permcode checks
@@ -75,7 +79,33 @@ async function createThread(task, env){
 	return new Response(id.toString() + ' ' +  resptext);
 }
 
-// todo: reply & delete
+async function replyThread(task, env){
+	// append reply to a thread.
+	if(task['threadId'] == undefined){
+		return new Response("Error: Replying to unknown message id.", {status: 404});
+	}
+	if(task['messageObj'] == undefined){
+		return new Response("messageObj not declared.", {status:404})
+	}
+	//todo: uAuth for reply
+	// should support uid/pas and maybe another quicker way.
+
+	let id;
+	try{
+		id = env.threaddb.idFromString(task['threadId']);
+	}catch(err){
+		return new Response(err.stack, {status: 404});
+	}
+	let stub = env.threaddb.get(id);
+
+	let taskstr = JSON.stringify({messageObj: task['messageObj']});
+	let req = new Request('reply', {method:"POST", body:taskstr});
+
+	let response = await stub.fetch(req); // send request
+	let resptext = await response.text(); // get returned text
+	return new Response(resptext);
+
+}
 
 
 // Define Durable object class
@@ -88,11 +118,26 @@ export class ThreadDB {
 
 	async initialize(){
 		// put post countings into object's memory.
-		let postcount = this.storage.get("postcount");
-		this.postcount = postcount || 0; // if postcount does not exist, it will be 0.
+		let lastpost = await this.storage.list({prefix: 'post-', limit: 1});
+		let postkey = Array.from(lastpost.keys())[0]; //get the only key name in this map.
+		if(postkey == undefined){
+			this.postcount = -1; // set to -1 for newly created objects.
+		} else {
+			this.postcount = parseInt(postkey.substring(5)); // remove the prefix and parse the Int.
+		}
 	}
 
 	async fetch(request){
+		// Make sure node is fully initialized from storage.
+		if( ! this.initializePromise){
+			this.initializePromise = this.initialize().catch((err) =>{
+				this.initializePromise = undefined;
+				throw err;
+			});
+		}
+		await this.initializePromise; // wait for it done.
+
+		// Accept requested action.
 		var path = (new URL(request.url)).pathname;
 		var task;
 		try{
@@ -104,9 +149,7 @@ export class ThreadDB {
 
 		if(path == '/create'){
 			//create new discussion
-			let metainfo = JSON.stringify(
-				{"creatorId": task['creatorId'], "title": task['title'], "properties": task['properties']}
-			)
+			let metainfo = {"creatorId": task['creatorId'], "title": task['title'], "properties": task['properties']};
 			//let storeObj = new Map();
 			//storeObj.set('meta', metainfo);
 			//storeObj.set('1', task['firstpost']);
@@ -114,7 +157,8 @@ export class ThreadDB {
 			try {
 				this.storage.put('meta', metainfo);
 				this.storage.put('post-1', task["firstpost"]);
-				return new Response("OK");
+				this.postcount = 1; // Add 1 to postcount.
+				return new Response("OK, " + this.postcount);
 			} catch(err){
 				return new Response(err.stack, {status: 404});
 			}
@@ -129,6 +173,19 @@ export class ThreadDB {
 			}
 			
 			return new Response(JSON.stringify(Object.fromEntries(list)));
+
+		} else if (path == '/reply'){
+			console.log(`Postcount Before: ${this.postcount}`);
+			// reply to a discussion (append a msg)
+			let msg = task['messageObj'];
+			let keyname = 'post-' + (this.postcount + 1); //since its the next post, not this last one.
+			try{
+				this.storage.put(keyname, msg);
+			}catch(err){
+				return new Response(err.stack, {status:404});
+			}
+			this.postcount ++;
+			return new Response(`OK, Key:${keyname}, Count:${this.postcount}`);
 		}
 
 		// Nothing should reach here.
